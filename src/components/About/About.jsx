582928eb60741b360199971c28947c14e46eb905
Prasadsing Solanki <prasadsingsolanki6006@gmail.com>
2025-09-14 12:19:49 +0530

    Update small screen view

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -54,13 +54,24 @@ const About = () => {
 
                 {/* Right Side */}
                 <div className="md:w-1/2 flex justify-center md:justify-end ">
-                    <Tilt className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"
-                    tiltMaxAngleX={70}
-                    tiltMaxAngleY={20}
-                    perspective={1000}
-                    scale={1.05}
-                    transitionSpeed={1000}
-                    gyroscope={true}>
+
+                    {/* For small screens → plain image */}
+                    <div className="block lg:hidden w-48 h-48 sm:w-64 sm:h-64 border-4 border-purple-700 rounded-full">
+                        <img
+                            src={Profile2}
+                            alt="Prasadsing Solanki"
+                            className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+                        />
+                    </div>
+
+                    {/* For large screens → Tilt effect */}
+                    <Tilt className="hidden lg:block w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"
+                        tiltMaxAngleX={70}
+                        tiltMaxAngleY={20}
+                        perspective={1000}
+                        scale={1.05}
+                        transitionSpeed={1000}
+                        gyroscope={true}>
                         <img
                             src={Profile2}
                             alt="Prasadsing Solanki"
